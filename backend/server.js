7b19062196fe574ceae1d70f0f022b02b6c60f52
Syed Javith <syedjavith14@gmail.com>
2023-08-28 19:51:39 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const connectToMongo = require('./db');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoute');
+const BodyParser = require('./middlewares/bodyParserMiddleware');
+const Cors = require('./middlewares/corsMiddleware');
+const StaticMiddleware = require('./middlewares/staticMiddleware');
+
+
+
+
+
+dotenv.config();
+const PORT = process.env.PORT
+const app = express();
+app.use(Cors);
+app.use(express.json());
+app.use(BodyParser);
+
+app.use(StaticMiddleware);
+app.use('/',userRoutes);
+
+
+connectToMongo();
+
+app.listen( PORT , ()=>{
+console.log("server started at port "+PORT);
+})
