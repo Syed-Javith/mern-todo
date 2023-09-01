ecafc87508f1ed4a29de28907e8ade209ee7b477
Syed Javith <syedjavith14@gmail.com>
2023-09-01 19:46:07 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter as Router, Routes , Route, Link} from "react-router-dom"
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Todo from './pages/Todo';
+import Error from './pages/Error';
+// import { useState , createContext} from 'react';
+
+
+// export const UserContext = createContext();
 
 function App() {
+  
+
+  // console.log(user);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <UserContext.Provider value={user}>
+    <Router>
+      <Link to='/' >Home</Link>
+      <Link to='/login' >Login</Link>
+      <Link to='/register' >Register</Link>
+      <Link to='/todo' >Todo</Link>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/todo' element={<Todo  />}/>
+        <Route path='/error' element={<Error/>}/>
+      </Routes>
+    </Router>
+    // </UserContext.Provider>
   );
 }
 
-export default App;
+export default App ;
